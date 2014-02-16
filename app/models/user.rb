0f61fed3011296187a542c7db139bddae56e621c
Tom Naessens <siloxtom@gmail.com>
2014-02-17 00:01:39 +0100

    Enable editing of accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,5 +21,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :recoverable,
-         :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable,
+         :registerable
 end
