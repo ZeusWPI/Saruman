89b33a43d4ba258659a0a74ab98ed13791af8e41
Tom Naessens <siloxtom@gmail.com>
2014-02-17 00:09:22 +0100

    And prohibited the sign up again

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Saruman::Application.routes.draw do
 
   # Devise
-  devise_for :users
+  devise_for :users, skip: [:registrations]
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
