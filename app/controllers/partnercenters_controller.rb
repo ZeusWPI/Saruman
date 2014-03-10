86753b584850a1eee2c094c847d6251b4139d5a1
Tom Naessens <Tom.Naessens@UGent.be>
2014-03-10 13:42:01 +0100

    Added models and stuff

--- app/controllers/partnercenters_controller.rb
+++ app/controllers/partnercenters_controller.rb
@@ -1,11 +1,8 @@
 class PartnercentersController < ApplicationController
   acts_as_token_authentication_handler_for Partner
 
-  def sign_in
-    redirect_to action: :show
-  end
-
   def show
+    @items = Items.all
   end
 
 end
---
+++ app/models/reservation.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: reservations
+#
+#  id           :integer          not null, primary key
+#  item_id      :integer
+#  partner_id   :integer
+#  count        :integer
+#  fetched      :integer
+#  brought_back :integer
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
+class Reservation < ActiveRecord::Base
+  belongs_to :item
+  belongs_to :partner
+end
