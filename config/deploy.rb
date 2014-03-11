d24001262cf6f5980e248072bde80ef440408f57
Tom Naessens <Tom.Naessens@UGent.be>
2014-03-11 14:19:42 +0100

    Add capistrano config

---
+++ config/deploy.rb
@@ -0,0 +1,58 @@
+# config valid only for Capistrano 3.1
+lock '3.1.0'
+
+set :application, 'Saruman'
+set :repo_url, 'git@github.com:ZeusWPI/Saruman.git'
+
+set :branch, 'master'
+set :deploy_to, '/home/saruman/production'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# Default deploy_to directory is /var/www/my_app
+# set :deploy_to, '/var/www/my_app'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+ set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    invoke 'puma:restart'
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,10 @@
+server 'zeus.ugent.be', user: 'saruman', roles: %w{web app db},
+  ssh_options: {
+  forward_agent: true,
+  auth_methods: ['publickey'],
+  port: 2222
+}
+
+set :rails_env, 'production'
+set :unicorn_pid, '/var/run/unicorn/saruman.pid'
+
