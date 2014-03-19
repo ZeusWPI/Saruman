41e50550c847659e27cf79bda0e7471a0755f0e0
Tom Naessens <Tom.Naessens@UGent.be>
2014-03-19 18:07:49 +0100

    Reservation helper tests

--- test/helpers/reservations_helper_test.rb
+++ test/helpers/reservations_helper_test.rb
@@ -1,4 +1,18 @@
 require 'test_helper'
 
 class ReservationsHelperTest < ActionView::TestCase
+
+  test "should get correct handler" do
+    r = reservations(:vtk_tent)
+
+    r.status = :approved
+    assert_match(/Approved/, status(r.status))
+
+    r.status = :pending
+    assert_match(/Pending/, status(r.status))
+
+    r.status = :disapproved
+    assert_match(/Disapproved/, status(r.status))
+  end
+
 end
