18a73956be5d73a27c281d2ab558fab0f4592396
Tom Naessens <Tom.Naessens@UGent.be>
2014-03-19 19:17:24 +0100

    Item model coverage

--- test/helpers/items_helper_test.rb
+++ test/helpers/items_helper_test.rb
@@ -1,4 +1,20 @@
 require 'test_helper'
 
 class ItemsHelperTest < ActionView::TestCase
+
+  test "item without price displays correctly" do
+    item = Item.new
+    item.name = "TestItem"
+    item.price = 0
+
+    assert_equal item.name_with_price, "TestItem"
+  end
+
+  test "item with price displays correctly" do
+    item = Item.new
+    item.name = "TestItem"
+    item.price = 10
+
+    assert_equal item.name_with_price, "#{item.name} - €#{'%0.2f' % item.price}"
+  end
 end
