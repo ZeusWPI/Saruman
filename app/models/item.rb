cad2d0b9db9d899cc0c9a53bd7edf3537213839c
Tom Naessens <Tom.Naessens@UGent.be>
2014-03-22 11:08:12 +0100

    Item name uniqueness

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@
 
 class Item < ActiveRecord::Base
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def price
