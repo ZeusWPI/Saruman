e06d0584cf7389b655dbbf784294925b9f38332e
Tom Naessens <Tom.Naessens@UGent.be>
2014-03-23 13:42:45 +0100

    Added papertrail changeset and view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140311141730) do
+ActiveRecord::Schema.define(version: 20140323122002) do
 
   create_table "items", force: true do |t|
     t.string   "name"
@@ -78,12 +78,13 @@ ActiveRecord::Schema.define(version: 20140311141730) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "versions", force: true do |t|
-    t.string   "item_type",  null: false
-    t.integer  "item_id",    null: false
-    t.string   "event",      null: false
+    t.string   "item_type",      null: false
+    t.integer  "item_id",        null: false
+    t.string   "event",          null: false
     t.string   "whodunnit"
     t.text     "object"
     t.datetime "created_at"
+    t.text     "object_changes"
   end
 
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
