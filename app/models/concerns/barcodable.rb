63c560ab28b113b9cd3da5cfc7d6d68655307568
Tom Naessens <Tom.Naessens@UGent.be>
2014-04-25 16:10:57 +0200

    This works too

--- app/models/concerns/barcodable.rb
+++ app/models/concerns/barcodable.rb
@@ -1,17 +1,11 @@
 module Barcodable
   extend ActiveSupport::Concern
 
-  module InstanceMethods
-    def generate_barcode
-      self.barcode_data = 12.times.map { SecureRandom.random_number(10) }.join
-      calculated_barcode = Barcodes.create('EAN13', data: self.barcode_data)
-      self.barcode = calculated_barcode.caption_data
-      self.save!
-    end
-  end
-
-  def self.included(base)
-    base.send :include, InstanceMethods
+  def generate_barcode
+    self.barcode_data = 12.times.map { SecureRandom.random_number(10) }.join
+    calculated_barcode = Barcodes.create('EAN13', data: self.barcode_data)
+    self.barcode = calculated_barcode.caption_data
+    self.save!
   end
 
 end
