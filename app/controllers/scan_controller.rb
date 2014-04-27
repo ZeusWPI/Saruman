b7cb5d22d68699d958bc26ac3a78fd3d804ed141
Tom Naessens <Tom.Naessens@UGent.be>
2014-04-27 18:15:34 +0200

    Hacky way to make sure no bad form can be resubmitted

--- app/controllers/scan_controller.rb
+++ app/controllers/scan_controller.rb
@@ -12,7 +12,8 @@ class ScanController < ApplicationController
     authorize! :manage, :all
 
     if blanks(params)
-      flash.now[:error] = "Please fill in all fields."
+      flash[:error] = "Please fill in all fields."
+      redirect_to action: :scan
     else
       @partner = User.partners.find_by_barcode(params.require(:scan)[:partner_code]) || User.partners.find_by_id(params.require(:scan)[:partner_id])
       @item = Item.find_by_id params.require(:scan)[:item_id]
@@ -20,15 +21,15 @@ class ScanController < ApplicationController
       if @partner and @item
         process_check
       else
-        flash.now[:error] = "The item or partner does not exist."
+        flash[:error] = "The item or partner does not exist."
+        redirect_to action: :scan
       end
-
     end
-
-    render action: :scan
   end
 
   def force
+    authorize! :manage, :all
+
     @partner = User.partners.find_by_id params.require(:force)[:partner_id]
     @item = Item.find_by_id params.require(:force)[:item_id]
     @count = params.require(:force)[:count].to_i
@@ -37,14 +38,14 @@ class ScanController < ApplicationController
     @reservation = @partner.reservations.approved.find_by_item_id @item.id
     if @reservation
       # Item found: increase and picked up count
-      flash.now[:notice] = "Increased the existing reservation with #{@reservation.picked_up_count + @count - @reservation.count}x #{@item.name}."
+      flash[:notice] = "Increased the existing reservation with #{@reservation.picked_up_count + @count - @reservation.count}x #{@item.name}."
 
       @reservation.count = @reservation.picked_up_count + @count
       @reservation.picked_up_count = @reservation.picked_up_count + @count
       @reservation.save
     else
       # No approved reservation for this item: add a new one
-      flash.now[:notice] = "Created a new reservation for #{@count}x #{@item.name}."
+      flash[:notice] = "Created a new reservation for #{@count}x #{@item.name}."
 
       @reservation = @partner.reservations.new
       @reservation.count = @count
@@ -54,7 +55,7 @@ class ScanController < ApplicationController
       @reservation.save
     end
 
-    render action: :scan
+    redirect_to action: :scan
   end
 
   private
@@ -64,6 +65,7 @@ class ScanController < ApplicationController
 
     if params.require(:scan)[:options] == "in"
       check_in params.require(:scan)
+      redirect_to action: :scan
     else
       check_out params.require(:scan)
     end
@@ -82,10 +84,10 @@ class ScanController < ApplicationController
       reservation.save
 
       # Notice the partner how many items he has left
-      flash.now[:notice] = "#{@partner.name} brought back #{@count}x #{@item.name}. He has #{reservation.picked_up_count - reservation.brought_back_count}x #{@item.name} remaining."
+      flash[:notice] = "#{@partner.name} brought back #{@count}x #{@item.name}. He has #{reservation.picked_up_count - reservation.brought_back_count}x #{@item.name} remaining."
     else
       # No reservations: display a warning
-      flash.now[:warning] = "#{@partner.name} does not have a reservation for this item."
+      flash[:warning] = "#{@partner.name} does not have a reservation for this item."
     end
   end
 
@@ -99,17 +101,20 @@ class ScanController < ApplicationController
         # If the new amount of picked up items exceeds the amount he has
         # reserved, enable the force on the page
         @display_force = true
+        render action: :scan
       else
         # Else, let him pick up these items
         @reservation.picked_up_count += @count
         @reservation.save
 
         # Notice the partner how many items he is allowed to pick up
-        flash.now[:notice] = "#{@partner.name} picked up #{@count}x #{@item.name}. He has still #{@reservation.count - @reservation.picked_up_count}x #{@item.name} remaining to pick up."
+        flash[:notice] = "#{@partner.name} picked up #{@count}x #{@item.name}. He has still #{@reservation.count - @reservation.picked_up_count}x #{@item.name} remaining to pick up."
+        redirect_to action: :scan
       end
     else
       # No reservation: show the force form on the scan page
       @display_force = true
+      render action: :scan
     end
   end
 
