88a3446988d76107148f079e01c5c735698505e1
Tom Naessens <Tom.Naessens@UGent.be>
2015-03-09 19:59:32 +0100

    Fixed url to 12urenloop, added king

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :deploy_to, '/home/saruman/production'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/initializers/secret_token.rb}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
