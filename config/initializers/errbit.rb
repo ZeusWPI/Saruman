fd656fc50b172c2fcfc878fd3da03d6df6bb8fbf
Tom Naessens <Tom.Naessens@UGent.be>
2015-03-16 21:40:00 +0100

    Migrate from errbit url

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,5 +1,6 @@
 Airbrake.configure do |config|
   config.api_key = '751337feac897fe3c0d004b28380ce86'
-  config.host    = 'errbit.nudded.be'
-  config.port    = 80
+  config.host    = 'zeus-errbit.ilion.me'
+  config.port    = 443
+  config.secure  = config.port == 443
 end
