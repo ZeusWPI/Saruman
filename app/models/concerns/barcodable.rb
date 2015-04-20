eaeb014501e9f1e99f30f4326cf85dff9a65d75a
Tom Naessens <Tom.Naessens@UGent.be>
2015-04-20 20:58:02 +0200

    Add paperclip, move before_create generate_barcode call

--- app/models/concerns/barcodable.rb
+++ app/models/concerns/barcodable.rb
@@ -1,6 +1,10 @@
 module Barcodable
   extend ActiveSupport::Concern
 
+  def self.included(base)
+    base.before_create :generate_barcode
+  end
+
   def generate_barcode
     self.barcode_data = 12.times.map { SecureRandom.random_number(10) }.join
     calculated_barcode = Barcodes.create('EAN13', data: self.barcode_data)
