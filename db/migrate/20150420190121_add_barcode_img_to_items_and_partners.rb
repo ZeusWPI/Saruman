ab2aede196d320d42522fde5cd83474cbd0effb8
Tom Naessens <Tom.Naessens@UGent.be>
2015-04-20 21:51:19 +0200

    Add barcodes to paperclip

---
+++ db/migrate/20150420190121_add_barcode_img_to_items_and_partners.rb
@@ -0,0 +1,28 @@
+class AddBarcodeImgToItemsAndPartners < ActiveRecord::Migration
+  require 'tempfile'
+
+  def set_barcode_img(m)
+    barcode = Barcodes.create('EAN13', data: m.barcode_data, bar_width: 35, bar_height: 1500, caption_height: 300, caption_size: 275 ) # required: height > size
+
+    tmpfile = Tempfile.new('barcode.png')
+    Barcodes::Renderer::Image.new(barcode).render(tmpfile)
+    tmpfile.rewind
+    m.barcode_img = tmpfile
+    tmpfile.close
+
+    m.save
+  end
+
+  def self.up
+    add_attachment :users, :barcode_img
+    add_attachment :items, :barcode_img
+
+    User.all.each { |u| set_barcode_img(u) }
+    Item.all.each { |i| set_barcode_img(i) }
+  end
+
+  def self.down
+    remove_attachment :users, :barcode_img
+    remove_attachment :items, :barcode_img
+  end
+end
