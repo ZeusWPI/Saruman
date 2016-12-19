575c5527023f892b650d75bd1f75ca3c789fe8b6
benji <benj_cous@hotmail.com>
2016-12-19 13:44:44 +0100

    New user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+
+  end
+
   def index
     @users = User.all
   end
@@ -8,5 +23,11 @@ class UsersController < ApplicationController
   def destroy
     User.find(params[:id]).destroy
     redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
