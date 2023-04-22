b4cf2a303e976441f4413bef85e5a879cb6ad878
Tom Naessens <silox@zeus.ugent.be>
2023-04-22 22:40:02 +0200

    Add seed for settings

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,5 @@
+# Create a settings object
+Settings.instance.update!(email: 'bestuur@zeus.ugent.be', event_name: 'Zeus', organisation_name: 'Zeus WPI')
+
+# Create an admin user
 User.create!(email: 'saruman@zeus.ugent.be', password: 'thewhite', role: :admin, name: 'Saruman The White')
