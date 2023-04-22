6f647e58d63e7060a4c8bf840818bcf302c70eeb
Tom Naessens <silox@zeus.ugent.be>
2023-04-22 21:35:21 +0200

    On second thought, let's go to 2.7.8

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,3 @@
-# Coverage testing
-require 'coveralls'
-Coveralls.wear!('rails')
-
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
