7d3e648c0388c3e8731157177a67d8e65b3a37b7
Tom Naessens <silox@zeus.ugent.be>
2024-02-17 18:33:17 +0100

    Polish up scan page

--- app/javascript/controllers/history_modal_controller.js
+++ app/javascript/controllers/history_modal_controller.js
@@ -2,12 +2,10 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   open() {
-    console.log("Sup")
     $("#history-modal").modal("show");
   }
 
   close() {
-    console.log("Sup closing")
     $("#history-modal").modal("hide");
   }
 }
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -8,3 +8,6 @@ application.register('disapprove-modal', DisapproveModal)
 
 import HistoryModal from './history_modal_controller'
 application.register('history-modal', HistoryModal)
+
+import IncrementButtons from './increment_buttons_controller'
+application.register('increment-buttons', IncrementButtons)
