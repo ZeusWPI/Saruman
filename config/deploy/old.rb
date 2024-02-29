25ff8b462c8944ab6952160e18c807f722a12d47
Tom Naessens <silox@zeus.ugent.be>
2024-02-29 20:15:56 +0100

    Last bibs and bobs

--- config/deploy/old.rb
+++
@@ -1,8 +0,0 @@
-server 'zeus.ugent.be', user: 'saruman', roles: %w{web app db},
-  ssh_options: {
-  forward_agent: true,
-  auth_methods: ['publickey'],
-  port: 2222
-}
-
-set :rails_env, 'production'
