0ad9f7b8a362815da834d707a1fb3a8706d601d0
Tom Naessens <silox@zeus.ugent.be>
2024-02-29 19:11:32 +0100

    Reservation scan stuff

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,5 +7,9 @@ import * as bootstrap from "bootstrap"
 
 $(document).on('ready turbo:load', function() {
   const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+  const _ = [...tooltipTriggerList].map(tooltipTriggerEl => {
+    if(!bootstrap.Tooltip.getInstance(tooltipTriggerEl)) {
+      new bootstrap.Tooltip(tooltipTriggerEl)
+    }
+  })
 });
