9cca861790de46d8fc7ecb65f51a91e67e13ce89
Tom Naessens <silox@zeus.ugent.be>
2024-05-26 17:51:04 +0200

    Set sentry context

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  include SentryUserContext
+
   after_action :store_location
 
   # Allows us to always replace the flash turbo frame when flashes are triggered
